3320d5d0278acd37c49577e700721c40e1b5a227
berluk12@gmail.com <berluk12@gmail.com>
2024-03-06 23:08:06 +0100

    Added filter to table with translations

--- src/main/java/com/bervan/languageapp/component/TranslationTable.java
+++ src/main/java/com/bervan/languageapp/component/TranslationTable.java
@@ -5,27 +5,45 @@ import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.grid.GridVariant;
 import com.vaadin.flow.component.html.Span;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.data.renderer.ComponentRenderer;
 import com.vaadin.flow.function.SerializableBiConsumer;
 import lombok.Getter;
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
+import java.util.*;
+import java.util.stream.Collectors;
 
 import static com.bervan.languageapp.component.ComponentCommonUtils.addAudioIfExist;
 
 @Component
 public class TranslationTable extends VerticalLayout {
-    private final Grid<TranslationRecord> grid;
+    private final Grid<TranslationRecord> grid = new Grid<>();
     @Getter
     private final List<Button> deleteButtons = new ArrayList<>();
+    private Map<UUID, String> recordsWithSearchValues;
+    private List<TranslationRecord> data;
 
     public TranslationTable(List<TranslationRecord> data) {
-        grid = new Grid<>();
+        refresh(data);
+        HorizontalLayout searchLayout = new HorizontalLayout();
+        Button searchButton = new Button("Search");
+        Button resetButton = new Button("Reset");
 
+        TextField searchField = new TextField();
+        searchField.addValueChangeListener(event -> search(data, event.getValue()));
+        searchButton.addClickListener(event -> search(data, searchField.getValue()));
+
+        resetButton.addClickListener(buttonClickEvent -> {
+            grid.setItems(this.data);
+            searchField.setValue("");
+        });
+
+        searchLayout.add(searchField);
+        searchLayout.add(searchButton);
+        searchLayout.add(resetButton);
         createButtons(data);
 
         grid.addColumn(createSourceTextWithSoundComponent()).setHeader("Text");
@@ -40,9 +58,31 @@ public class TranslationTable extends VerticalLayout {
 
         grid.getColumns().forEach(column -> column.setHeader(new Span(column.getHeaderText())));
 
-        grid.setItems(data);
-
+        add(searchLayout);
         add(grid);
+    }
+
+    private void search(List<TranslationRecord> data, String value) {
+        if (value.length() > 2) {
+            Set<UUID> matches = this.recordsWithSearchValues.entrySet().stream()
+                    .filter(e -> e.getValue().contains(value.toLowerCase()))
+                    .map(Map.Entry::getKey)
+                    .collect(Collectors.toSet());
+
+            Set<TranslationRecord> filteredValue = data.stream().filter(e -> matches.contains(e.getUuid()))
+                    .collect(Collectors.toSet());
+            grid.setItems(filteredValue);
+        }
+    }
+
+    private Map<UUID, String> buildRecordsWithSearchValues(List<TranslationRecord> data) {
+        Map<UUID, String> recordWithSearchValues = new HashMap<>();
+        for (TranslationRecord d : data) {
+            String search = Arrays.asList(d.getSourceText(), d.getTextTranslation(), d.getInSentence(), d.getInSentenceTranslation()).toString();
+            recordWithSearchValues.put(d.getUuid(), search.toLowerCase());
+        }
+
+        return recordWithSearchValues;
     }
 
     private void createButtons(List<TranslationRecord> data) {
@@ -90,6 +130,8 @@ public class TranslationTable extends VerticalLayout {
     }
 
     public void refresh(List<TranslationRecord> data) {
+        this.data = data;
         grid.setItems(data);
+        this.recordsWithSearchValues = buildRecordsWithSearchValues(data);
     }
 }
