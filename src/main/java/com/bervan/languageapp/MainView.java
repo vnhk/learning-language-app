2b0605baf9af4cd34be45ea7960e689483508eef
berluk12@gmail.com <berluk12@gmail.com>
2024-03-10 09:57:27 +0100

    Added useful pages

--- src/main/java/com/bervan/languageapp/MainView.java
+++ src/main/java/com/bervan/languageapp/MainView.java
@@ -2,13 +2,16 @@ package com.bervan.languageapp;
 
 import com.bervan.languageapp.component.Form;
 import com.bervan.languageapp.component.TranslationTable;
+import com.google.common.collect.ImmutableMap;
 import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.html.Anchor;
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
 
@@ -16,6 +19,7 @@ import java.util.UUID;
 public class MainView extends VerticalLayout {
     private final TranslationRecordService translatorRecordService;
     private List<TranslationRecord> translations;
+    private Map<String, String> helpfulLinks = ImmutableMap.of("https://youglish.com", "The page that fins words in youtube videos");
 
     public MainView(TranslationRecordService translatorRecordService,
                     ExampleOfUsageService exampleOfUsageService,
@@ -31,6 +35,8 @@ public class MainView extends VerticalLayout {
             UI.getCurrent().navigate("learning");
         });
 
+        buildHelpfulPagesLinks();
+
         add(learnButtonPage, new H1("Translations"), form, translationTable);
 
         for (Button deleteButton : translationTable.getDeleteButtons()) {
@@ -39,6 +45,13 @@ public class MainView extends VerticalLayout {
                 translations = translatorRecordService.getAll();
                 translationTable.refresh(translations);
             });
+        }
+    }
+
+    private void buildHelpfulPagesLinks() {
+        for (Map.Entry<String, String> stringStringEntry : helpfulLinks.entrySet()) {
+            Anchor a = new Anchor(stringStringEntry.getKey(), "-" + stringStringEntry.getValue() + "(" + stringStringEntry.getKey() + ")");
+            add(a);
         }
     }
 }
