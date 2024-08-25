d3f11888bd7d201fe6330e25b811d6eecba38234
berluk12@gmail.com <berluk12@gmail.com>
2024-08-25 18:40:09 +0200

    Translation API logging

--- src/main/java/com/bervan/languageapp/service/TranslatorService.java
+++ src/main/java/com/bervan/languageapp/service/TranslatorService.java
@@ -1,5 +1,6 @@
 package com.bervan.languageapp.service;
 
+import com.bervan.common.model.BervanLogger;
 import com.google.cloud.translate.Translate;
 import com.google.cloud.translate.TranslateOptions;
 import com.google.cloud.translate.Translation;
@@ -10,15 +11,22 @@ import org.springframework.stereotype.Service;
 public class TranslatorService {
     @Value("${translation.api.key}")
     private String API_KEY;
-    private final Translate translate;
+    private Translate translate;
+    private final BervanLogger logger;
 
-    public TranslatorService() {
+    public TranslatorService(BervanLogger logger) {
+        this.logger = logger;
+
         translate = TranslateOptions.newBuilder()
                 .setApiKey(API_KEY)
                 .build().getService();
+
+        logger.logDebug("TRANSLATE_API_KEY:" + API_KEY);
     }
 
     public String translate(String value, String sourceLanguage, String targetLanguage) {
+        logger.logDebug("TRANSLATE_API_KEY:" + API_KEY);
+
         Translation translation = translate.translate(value,
                 Translate.TranslateOption.sourceLanguage(sourceLanguage),
                 Translate.TranslateOption.targetLanguage(targetLanguage));
