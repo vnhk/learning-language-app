e20e1cc0281125d95668951f32a75bb607156d87
berluk12@gmail.com <berluk12@gmail.com>
2024-12-25 15:08:14 +0100

    Navigation menu addButtonIfVisible

--- src/main/java/com/bervan/languageapp/view/LearningEnglishLayout.java
+++ src/main/java/com/bervan/languageapp/view/LearningEnglishLayout.java
@@ -1,15 +1,14 @@
 package com.bervan.languageapp.view;
 
 import com.bervan.common.MenuNavigationComponent;
-import com.vaadin.flow.component.html.Hr;
 
 public final class LearningEnglishLayout extends MenuNavigationComponent {
     public LearningEnglishLayout(String routeName) {
         super(routeName);
 
-        addButton(menuButtonsRow, AbstractLearningAppHomeView.ROUTE_NAME, "Home");
-        addButton(menuButtonsRow, AbstractLearningView.ROUTE_NAME, "Flashcards");
-        addButton(menuButtonsRow, AbstractQuizView.ROUTE_NAME, "Quiz");
+        addButtonIfVisible(menuButtonsRow, AbstractLearningAppHomeView.ROUTE_NAME, "Home");
+        addButtonIfVisible(menuButtonsRow, AbstractLearningView.ROUTE_NAME, "Flashcards");
+        addButtonIfVisible(menuButtonsRow, AbstractQuizView.ROUTE_NAME, "Quiz");
 
         add(menuButtonsRow);
     }
