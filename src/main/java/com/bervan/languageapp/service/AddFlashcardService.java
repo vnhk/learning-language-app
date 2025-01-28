f4d06e405c8e2a76a7fd4edd909779450f884914
berluk12@gmail.com <berluk12@gmail.com>
2025-01-28 22:40:22 +0100

    async add as flashcard

--- src/main/java/com/bervan/languageapp/service/AddFlashcardService.java
+++ src/main/java/com/bervan/languageapp/service/AddFlashcardService.java
@@ -2,6 +2,8 @@ package com.bervan.languageapp.service;
 
 import com.bervan.common.model.PersistableTableData;
 import com.bervan.languageapp.TranslationRecord;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -20,42 +22,45 @@ public class AddFlashcardService {
         this.textToSpeechService = textToSpeechService;
     }
 
-    public void addAsFlashcard(PersistableTableData item) {
-        String name = item.getTableFilterableColumnValue();
-        String translated = translatorService.translate(name);
-        List<String> exampleOfUsage = exampleOfUsageService.createExampleOfUsage(name);
-        String examples = exampleOfUsage.toString().replace("[", "").replace("]", "");
+    public void addAsFlashcardAsync(PersistableTableData item) {
+        SecurityContext context = SecurityContextHolder.getContext();
+        new Thread(() -> {
+            SecurityContextHolder.setContext(context);
+            String name = item.getTableFilterableColumnValue();
+            String translated = translatorService.translate(name);
+            List<String> exampleOfUsage = exampleOfUsageService.createExampleOfUsage(name);
+            String examples = exampleOfUsage.toString().replace("[", "").replace("]", "");
 
-        TranslationRecord record = new TranslationRecord();
-        record.setSourceText(name);
-        record.setTextTranslation(translated);
-        record.setFactor(1);
-        if (!examples.isBlank()) {
-            if (examples.length() > 500) {
-                StringBuilder builder = new StringBuilder();
-                for (String s : exampleOfUsage) {
-                    if (builder.length() + s.length() + 1 > 500) {
-                        break;
+            TranslationRecord record = new TranslationRecord();
+            record.setSourceText(name);
+            record.setTextTranslation(translated);
+            record.setFactor(1);
+            if (!examples.isBlank()) {
+                if (examples.length() > 500) {
+                    StringBuilder builder = new StringBuilder();
+                    for (String s : exampleOfUsage) {
+                        if (builder.length() + s.length() + 1 > 500) {
+                            break;
+                        }
+                        builder.append(s);
+                        builder.append(",");
                     }
-                    builder.append(s);
-                    builder.append(",");
+                    examples = builder.substring(0, builder.length() - 2);
                 }
-                examples = builder.substring(0, builder.length() - 2);
+
+                record.setInSentence(examples);
+                String examplesTranslated = translatorService.translate(examples);
+                record.setInSentenceTranslation(examplesTranslated);
+
+                if (record.getSourceText() != null && !record.getSourceText().isBlank()) {
+                    record.setTextSound(textToSpeechService.getTextSpeech(record.getSourceText()));
+                }
+
+                if (record.getInSentence() != null && !record.getInSentence().isBlank()) {
+                    record.setInSentenceSound(textToSpeechService.getTextSpeech(record.getInSentence()));
+                }
             }
-
-            record.setInSentence(examples);
-            String examplesTranslated = translatorService.translate(examples);
-            record.setInSentenceTranslation(examplesTranslated);
-
-            if (record.getSourceText() != null && !record.getSourceText().isBlank()) {
-                record.setTextSound(textToSpeechService.getTextSpeech(record.getSourceText()));
-            }
-
-            if (record.getInSentence() != null && !record.getInSentence().isBlank()) {
-                record.setInSentenceSound(textToSpeechService.getTextSpeech(record.getInSentence()));
-            }
-        }
-        translationRecordService.save(record);
-
+            translationRecordService.save(record);
+        }).start();
     }
 }
