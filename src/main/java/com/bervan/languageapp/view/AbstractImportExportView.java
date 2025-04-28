75250335b25895f9c2c8ea6c8056077b5e15948a
berluk12@gmail.com <berluk12@gmail.com>
2025-04-28 18:38:10 +0200

    Refactor

--- src/main/java/com/bervan/languageapp/view/AbstractImportExportView.java
+++ src/main/java/com/bervan/languageapp/view/AbstractImportExportView.java
@@ -3,15 +3,15 @@ package com.bervan.languageapp.view;
 import com.bervan.common.AbstractDataIEView;
 import com.bervan.core.model.BervanLogger;
 import com.bervan.languageapp.TranslationRecord;
-import com.bervan.languageapp.service.ExampleOfUsageService;
 import com.bervan.languageapp.service.TranslationRecordService;
 
 import java.util.Collections;
+import java.util.UUID;
 
-public abstract class AbstractImportExportView extends AbstractDataIEView {
-    public static final String ROUTE_NAME = "learning-english-app/ie";
+public abstract class AbstractImportExportView extends AbstractDataIEView<UUID> {
+    public static final String ROUTE_NAME = "learning-english-app/import-export";
 
-    public AbstractImportExportView(BervanLogger logger, TranslationRecordService translationRecordService, ExampleOfUsageService exampleOfUsageService) {
+    public AbstractImportExportView(BervanLogger logger, TranslationRecordService translationRecordService) {
         super(Collections.singletonList(translationRecordService),
                 new LearningEnglishLayout(ROUTE_NAME),
                 logger,
