f47909437e0c653e2dca7fe61bc8e2015cb0afcd
berluk12@gmail.com <berluk12@gmail.com>
2025-04-28 21:55:16 +0200

    Common AbstractFilters view, import export data fix, out of memory fix

--- src/main/java/com/bervan/languageapp/view/AbstractImportExportView.java
+++ src/main/java/com/bervan/languageapp/view/AbstractImportExportView.java
@@ -5,16 +5,15 @@ import com.bervan.core.model.BervanLogger;
 import com.bervan.languageapp.TranslationRecord;
 import com.bervan.languageapp.service.TranslationRecordService;
 
-import java.util.Collections;
 import java.util.UUID;
 
-public abstract class AbstractImportExportView extends AbstractDataIEView<UUID> {
+public abstract class AbstractImportExportView extends AbstractDataIEView<UUID, TranslationRecord> {
     public static final String ROUTE_NAME = "learning-english-app/import-export";
 
     public AbstractImportExportView(BervanLogger logger, TranslationRecordService translationRecordService) {
-        super(Collections.singletonList(translationRecordService),
+        super(translationRecordService,
                 new LearningEnglishLayout(ROUTE_NAME),
                 logger,
-                Collections.singletonList(TranslationRecord.class));
+                TranslationRecord.class);
     }
 }
