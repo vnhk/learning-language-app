734725367273afdc83bdabf271c5e40726a07668
berluk12@gmail.com <berluk12@gmail.com>
2025-08-17 14:27:17 +0200

    New Navigation menu styling

--- src/main/java/com/bervan/languageapp/view/LearningEnglishLayout.java
+++ src/main/java/com/bervan/languageapp/view/LearningEnglishLayout.java
@@ -1,16 +1,17 @@
 package com.bervan.languageapp.view;
 
 import com.bervan.common.MenuNavigationComponent;
+import com.vaadin.flow.component.icon.VaadinIcon;
 
 public final class LearningEnglishLayout extends MenuNavigationComponent {
     public LearningEnglishLayout(String routeName) {
         super(routeName);
 
-        addButtonIfVisible(menuButtonsRow, AbstractLearningAppHomeView.ROUTE_NAME, "Home");
-        addButtonIfVisible(menuButtonsRow, AbstractLearningView.ROUTE_NAME, "Flashcards");
-        addButtonIfVisible(menuButtonsRow, AbstractQuizView.ROUTE_NAME, "Quiz");
-        addButtonIfVisible(menuButtonsRow, AbstractCrosswordView.ROUTE_NAME, "Crossword");
-        addButtonIfVisible(menuButtonsRow, AbstractImportExportView.ROUTE_NAME, "Data IE");
+        addButtonIfVisible(menuButtonsRow, AbstractLearningAppHomeView.ROUTE_NAME, "Home", VaadinIcon.HOME.create());
+        addButtonIfVisible(menuButtonsRow, AbstractLearningView.ROUTE_NAME, "Flashcards", VaadinIcon.HOME.create());
+        addButtonIfVisible(menuButtonsRow, AbstractQuizView.ROUTE_NAME, "Quiz", VaadinIcon.HOME.create());
+        addButtonIfVisible(menuButtonsRow, AbstractCrosswordView.ROUTE_NAME, "Crossword", VaadinIcon.HOME.create());
+        addButtonIfVisible(menuButtonsRow, AbstractImportExportView.ROUTE_NAME, "Data IE", VaadinIcon.HOME.create());
 
         add(menuButtonsRow);
     }
