ebd089a8767f39bb37c792173b887e5b3a98f709
berluk12@gmail.com <berluk12@gmail.com>
2025-12-14 17:01:49 +0100

    Convert entities to ownership-aware base classes.
    Transitioned from `BervanBaseEntity` and `BervanHistoryEntity` to `BervanOwnedBaseEntity` and `BervanHistoryOwnedEntity` across the project. Updated related interfaces and logic to support ownership management and access control.

--- src/main/java/com/bervan/languageapp/TranslationRecord.java
+++ src/main/java/com/bervan/languageapp/TranslationRecord.java
@@ -1,7 +1,7 @@
 package com.bervan.languageapp;
 
-import com.bervan.common.model.BervanBaseEntity;
-import com.bervan.common.model.PersistableTableData;
+import com.bervan.common.model.BervanOwnedBaseEntity;
+import com.bervan.common.model.PersistableTableOwnedData;
 import com.bervan.ieentities.ExcelIEEntity;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Size;
@@ -14,7 +14,7 @@ import java.util.UUID;
 
 @Entity
 @Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
-public class TranslationRecord extends BervanBaseEntity<UUID> implements PersistableTableData<UUID>, ExcelIEEntity<UUID> {
+public class TranslationRecord extends BervanOwnedBaseEntity<UUID> implements PersistableTableOwnedData<UUID>, ExcelIEEntity<UUID> {
     public static final String TranslationRecord_sourceText_columnName = "sourceText";
     public static final String TranslationRecord_markedForLearning_columnName = "markedForLearning";
     public static final String TranslationRecord_textTranslation_columnName = "textTranslation";
